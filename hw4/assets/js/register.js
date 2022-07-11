4bfde0e11e8f067e0b1f0a5f3225021087f7989b
GamDang199 <gamdth.b19at051@stu.ptit.edu.vn>
2022-07-11 11:00:32 +0700

    hw4 v2

---
+++ hw4/assets/js/register.js
@@ -0,0 +1,73 @@
+var firstname = document.querySelector('#firstname');
+var lastname = document.querySelector('#lastname');
+var username = document.querySelector('#username');
+var email = document.querySelector('#email');
+var password = document.querySelector('#password');
+var confirmPassword = document.querySelector('#confirmPassword');
+var form = document.querySelector('#form');
+
+const showError = (input, message) => {
+    let parent = input.parentElement;
+    let err = parent.querySelector('span')
+    parent.classList.add('error');
+    err.innerText =  message;
+}
+
+const showSuccess = (input) => {
+    let parent = input.parentElement;
+    let err = parent.querySelector('span')
+    parent.classList.remove('error');
+    err.innerText = '';
+}
+
+const checkEmptyError = (listInput) => {
+    let isEmptyError = false;
+    listInput.forEach(input => {
+        input.value = input.value.trim();
+        if(!input.value) {
+            isEmptyError = true;
+            showError(input, 'Khong duoc de trong');
+        } else {
+            showSuccess(input)
+        }
+    });
+    return isEmptyError;
+}
+
+const checkEmail = (input) => {
+    const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    input.value = input.value.trim();
+    let isEmailError = !regexEmail.test(input.value);
+    if(regexEmail.test(input.value)) {
+        showSuccess(input.value);
+    } else {
+        showError(email, 'Email khong dung dinh dang');
+    }
+    return isEmailError;
+}
+
+const checkLength = (input, min) => {
+    input.value = input.value.trim();
+    if(input.value.length < min) {
+        showError(input, `Phai co it nhat ${min} ky tu`);
+        return true;
+    }
+    showSuccess();
+    return false;
+}
+
+const checkMatchPassword = (passwordInput, cPasswordInput) => {
+    if(passwordInput.value.trim() !== cPasswordInput.value.trim()) {
+        showError(cPasswordInput, 'Mat khau khong trung khop');
+        return true;
+    }
+    return false;
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let isEmptyError = checkEmptyError([firstname, lastname, username, email, password, confirmPassword]);
+    let isEmailError = checkEmail(email);
+    let checkLengthPassword = checkLength(password, 8);
+    let isMatchPass = checkMatchPassword(password, confirmPassword);
+})
